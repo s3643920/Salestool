9184e5cebd541c65b3913e15e57069b28375d850
s3643920 <s3643920@student.rmit.edu.au>
2017-08-30 07:17:24 +0530

    Added code

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,18 @@
+
+package ca.demo.salestool;
+
+public class Main {
+public static void main(String[] args) {
+	SalesData data=new SalesData();
+	displaygreeting();
+	data.display();
+}
+	private static void displaygreeting() {
+System.out.println("Hello Happy sales people!");
+System.out.println("This app shows sales data");
+System.out.println("----------------------------");
+
+
+	}
+}
+
